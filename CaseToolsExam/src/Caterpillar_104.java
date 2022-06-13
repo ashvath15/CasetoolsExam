16636cad434457e50fb42e86b1cf2c447c47dd0d
admin <admin@Bcalab4-m01>
2022-06-13 09:26:02 +0530

    Commit 2

--- CaseToolsExam/src/Caterpillar_104.java
+++ CaseToolsExam/src/Caterpillar_104.java
@@ -3,11 +3,14 @@ public class Caterpillar_104 {
 	int moves;
 	int cabbages;
 	
-	Caterpillat_104(int moves,int cabbages){
+	int Caterpillat_104(int moves,int cabbages){
 		this.moves=moves;
 		this.cabbages=cabbages;
+		return 0;
 	}
-	
+	int get_moves() {
+		return moves;
+	}
 	
 	public static void main(int moves,int cabbages) {
 		// TODO Auto-generated method stub
